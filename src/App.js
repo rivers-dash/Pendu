82d16bdeb739294a9ee3d69bd7c4efb769d61ecb
SmailMeziani <smeziani@bk-consulting.com>
2018-10-02 13:59:55 +0200

    :sparkles: Improving generateWord

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import Button from './Button.js';
 
 const LINE = 14
-const WORD = 'HIDDEN WORD forever and ever together'
-const HIDDEN_SYMBOL = '_'
+const WORD = 'permutation THE WORLD SHOULD BE A BETTER PLACE'
 const LETTERS = (['A','Z','E','R','T','Y','U','I','O','P','Q','S','D','F','G','H','J','K','L','M','W','X','C','V','B','N'])
 
 
@@ -16,27 +15,38 @@ class App extends Component {
 	}
 
 	generateWord() {
-		const { word, size } = this.state
-		const hiddenWord = []
-		let temp = []
-		let lines = word
-		let a = 0
-		let b = LINE
-		let parcourir = 0
-		let lastSpace
-		let line
-		console.log('size : ', size)
+		let { word, size } = this.state
+		let v1 = []
+		let spaceIndex
+		let hiddenWord = []
+		let lines = []
 
-		while (parcourir < size) {
-			lines = word.slice(a, b)
-			lastSpace = lines.lastIndexOf(' ')
-			console.log('lines : ', lines)
-			console.log('last space : ', lastSpace)
+		// Séparation des mots
+		while (spaceIndex !== -1) {
+			spaceIndex = word.indexOf(' ')
 
-			console.log('ANSWER : ', word.slice(a, a+lastSpace))
-			line = word.slice(a, a+lastSpace)
+			if (spaceIndex !== -1){
+				v1.push(word.slice(0, spaceIndex))
+			}
+			else {
+				v1.push(word.slice(0))
+			}
 
-			for (var i=0; i<lastSpace; i++) {
+			word = word.slice(spaceIndex + 1, size)
+
+		}
+
+		for (var i=0; i<v1.length - 1; i++) {
+			if (v1[i].length + v1[i+1].length < LINE) {
+				v1[i] = v1[i].concat(' ', v1[i+1])
+				v1.splice(i+1, 1)
+				i--
+			}
+		}
+
+		v1.forEach(function(line) {
+			hiddenWord = []
+			for (var i=0; i<line.length; i++) {
 				if (line[i] !== ' ') {
 					hiddenWord.push({
 						index: i+1,
@@ -53,39 +63,10 @@ class App extends Component {
 					})
 				}
 			}
+			lines.push(hiddenWord)
+		})
 
-
-			a = a + lastSpace
-			b = a + LINE
-			parcourir = parcourir + LINE
-
-			console.log('a : ',a)
-			console.log('b : ',b)
-			console.log('parcourir : ', parcourir)
-			console.log('________________________')
-		}
-
-		for (var i=0; i<size; i++) {
-			if (word[i] !== ' ') {
-				hiddenWord.push({
-					index: i+1,
-					value: word[i],
-					feedback: 'hidden'
-				})
-			}
-
-			else {
-				hiddenWord.push({
-					index: i+1,
-					value: word[i],
-					feedback: 'visible'
-				})
-			}
-		}
-
-		console.log(hiddenWord)
-
-		return hiddenWord
+		return lines
 	}
 
   render() {
@@ -93,14 +74,25 @@ class App extends Component {
 			<div className="pendu">
 
 				<div className="hiddenWord">
-					{this.generateWord().map((letter, index) => (
-						<Button
-							key={index}
-							letter={letter.value}
-							feedback={letter.feedback}
-							index={letter.index}
-						/>
-					))}
+
+					{
+						this.generateWord().map((line) => (
+							<div className="hiddenWord">
+								{
+									line.map((letter, index) => (
+										<Button
+											key={index}
+											letter={letter.value}
+											feedback={letter.feedback}
+											index={letter.index}
+										/>
+
+									))
+								}
+							</div>
+						))
+					}
+
 				</div>
 
 				<div className="pendu">
