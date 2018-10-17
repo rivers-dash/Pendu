88b46d3868dbc8af23acf61ee1748218fa06dd04
SmailMeziani <smeziani@bk-consulting.com>
2018-10-17 11:11:10 +0200

    :sparkles: [Icons] Adding icons component thanks to react-icons

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import Navbar from './components/navbar/navbar'
 
 class App extends Component {
 	state = {
+		title: 'Hangman'
 	}
 
   render() {
+		const { title } = this.state
     return(
 			<div id="app" className="app cover-container d-flex w-100 h-100 p-3 flex-column full-height-div">
 				<div id="appContainer" class="full-height-div">
-					<Navbar></Navbar>
+					<Navbar
+						title={title}
+					></Navbar>
 				</div>
 			</div>
 		)
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './navbar.css'
 
+import { FaMale, FaTrophy ,FaPlus } from 'react-icons/fa'
+
 class Navbar extends Component {
 	state = {
 	}
@@ -10,11 +12,24 @@ class Navbar extends Component {
 			<div >
 				<header className="masthead mb-auto mt-2">
         <div className="inner row">
-          <h3 className="masthead-brand flex-grow-1">Hangman</h3>
+          <h3 className="masthead-brand flex-grow-1">{this.props.title}</h3>
           <nav className="nav nav-pills pill justify-content-end">
-            <a className="nav-link active" href="#">Home</a>
-            <a className="nav-link" href="#">Features</a>
-            <a className="nav-link" href="#">Contact</a>
+
+            <a className="nav-link active" href="#">
+							<FaMale className="justify-content-center col"/>
+							<div className="justify-content-center col">Play</div>
+						</a>
+
+						<a className="nav-link" href="#">
+							<FaTrophy className="justify-content-center col"/>
+							<div className="justify-content-center col">Hi-Scores</div>
+						</a>
+
+						<a className="nav-link" href="#">
+							<FaPlus className="justify-content-center col"/>
+							<div className="justify-content-center col">Add words</div>
+						</a>
+
           </nav>
         </div>
       </header>
